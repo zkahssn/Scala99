f98e7948c449145099751d2a09a054517337e16a
zhossain <zakia.hossain@episerver.com>
2019-02-01 16:22:37 +0000

    question 12

--- src/FindLast.scala
+++ src/FindLast.scala
@@ -1,28 +1,134 @@
+
+import scala.collection.mutable.ListBuffer
+
 class FindLast {
 
-  val x: List[Int] = List(1, 1, 2, 3, 5, 8);
   val y: List[Int] = List(1, 2, 3, 2, 1);
+  val z: List[Int] = List(1, 1);
+  val b: List[Int] = List(2, 3);
+  val c: List[Int] = List(5, 8);
+  val a: List[List[Int]] = List(z, b);
+  val toFlatten: List[Seq[Int]] = List(z, b, c);
+  var duplicate: List[Symbol] = List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
 
-  def findLast(): Int = {
-    return x.reverse.head
-  }
-
-  def findPenultimate(): Int = {
-    return x.reverse.tail.head
-  }
-
-  def numOfElements(): Int = {
-    return x.length
-  }
-
-  def reverseList(): List[Int] = {
-    return x.reverse
+  //1
+  def findLast(list: List[Int]): Int = {
+    return list.reverse.head
   }
 
-  def isPalindrome(): Boolean = {
-  if(y == y.reverse){
-    return true
+  //2
+  def findPenultimate(list: List[Int]): Int = {
+    return list.reverse.tail.head
   }
-    return  false
+
+  //3 not complete
+  //4
+  def numOfElements(list: List[Int]): Int = {
+    return list.length
   }
+
+  //5
+  def reverseList(list: List[Int]): List[Int] = {
+    return list.reverse
+  }
+
+  //6
+  def isPalindrome(list: List[Int]): Boolean = {
+    if (list == list.reverse) {
+      return true
+    }
+    return false
+  }
+
+  //7
+  def flattenList(flatten: List[Seq[Int]]): List[Int] = {
+    return flatten.flatten
+  }
+
+  //8
+  def compress(duplicate: List[Symbol]): List[Symbol] = {
+    var distinct: ListBuffer[Symbol] = ListBuffer(duplicate.head)
+    for (d <- duplicate) {
+      if (!distinct.last.equals(d)) {
+        distinct += d
+      }
+    }
+    return distinct.toList
+  }
+
+  //9
+  def pack(duplicate: List[Symbol]): List[List[Symbol]] = {
+    val distinctOrder: List[Symbol] = compress(duplicate)
+    val packedOrders: ListBuffer[List[Symbol]] = ListBuffer()
+    val lastElement: Int = duplicate.lastIndexOf(duplicate.last)
+    var repeat: ListBuffer[Symbol] = ListBuffer()
+    duplicate.zipWithIndex.foreach { case (x, y) =>
+      if (y.equals(0)) {
+        repeat += x
+      }
+      else if (x.equals(repeat.last)) {
+        repeat += x
+      }
+      if (!x.equals(repeat.last) || (y == (lastElement))) {
+        packedOrders.append(repeat.toList)
+        repeat = ListBuffer(x)
+      }
+    }
+
+    return packedOrders.toList
+  }
+
+  //10
+  def encode(encodeList: List[Symbol]): List[(Int, Symbol)] = {
+    val packedList = pack(encodeList)
+    var encodedList: ListBuffer[(Int, Symbol)] = ListBuffer()
+    packedList.foreach(item => {
+      val itemSize: Int = item.size
+      encodedList.append((itemSize, item.last))
+    }
+    )
+    return encodedList.toList
+
+  }
+
+  //11
+  def encodeDirect(duplicates: List[Symbol]): List[Any] = {
+    val packedList = pack(duplicates)
+    var encodedList: ListBuffer[Any] = ListBuffer()
+    packedList.foreach(item => {
+      if (item.size == 1) {
+        encodedList.append(item.last)
+      }
+      else {
+        encodedList.append((item.size, item.last))
+      }
+
+    })
+    return encodedList.toList
+  }
+
+  //12
+
+  def decode(encoded: List[(Int, Symbol)]): List[Symbol] = {
+    var decodedList: ListBuffer[List[Symbol]] = ListBuffer();
+    encoded.foreach(item => {
+      val currentList: List[Symbol] = List.fill(item._1)(item._2)
+      decodedList += List.fill(item._1)(item._2)
+    })
+
+
+    return decodedList.toList.flatten
+  }
+
+  //13
+  def directEncode(list: List[Any]): List[(Int, Symbol)] = {
+
+    list.foreach(item => {
+
+    })
+
+    return null
+  }
+
+
 }
--- src/Main.scala
+++ src/Main.scala
@@ -1,9 +1,24 @@
 object Main extends App {
+  val x: List[Int] = List(1, 1, 2, 3, 5, 8);
+  val y: List[Int] = List(1, 2, 3, 2, 1);
+  val z: List[Int] = List(1, 1);
+  val b: List[Int] = List(2,3);
+  val c: List[Int] = List(5,8);
+  val toFlatten : List[Seq[Int]] = List(z,b,c);
+  var duplicate : List[Symbol] = List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
+
+
   val listClass = new FindLast;
-  listClass.findLast();
-  println(listClass.findLast())
-  println(listClass.findPenultimate())
-  println(listClass.numOfElements())
-  println(listClass.reverseList())
-  print(listClass.isPalindrome())
+  println(listClass.findLast(x))
+  println(listClass.findPenultimate(x))
+  println(listClass.numOfElements(x))
+  println(listClass.reverseList(x))
+  println(listClass.isPalindrome(y))
+  println(listClass.flattenList(toFlatten))
+  println(listClass.compress(duplicate))
+  println(listClass.pack(duplicate))
+  println(listClass.encode(duplicate))
+  println(listClass.encodeDirect(duplicate))
+  println(listClass.decode(listClass.encode(duplicate)))
+
 }
